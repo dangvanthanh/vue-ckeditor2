8dec1c6bc7071fef517efb7f7bceafda4567a454
Dang Van Thanh <dangvanthanh@dangthanh.org>
2021-06-07 23:44:48 +0700

    Update demo for next vue-ckeditor2

---
+++ App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{ bundler }}</h1>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default {
+  data() {
+    return {
+      bundler: 'Ckeditor',
+    };
+  },
+};
+</script>
+
+<style  scoped>
+.container {
+  color: green;
+}
+</style>
